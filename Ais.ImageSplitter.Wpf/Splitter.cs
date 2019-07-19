9243ecbfaa0cc04c3b29c6943fd785d3f3ecd135
Lee Dumond <Lee_Dumond@aisreview.com>
2019-07-19 08:38:28 -0500

    use IAsyncDisposable

--- Ais.ImageSplitter.Wpf/Splitter.cs
+++ Ais.ImageSplitter.Wpf/Splitter.cs
@@ -11,45 +11,42 @@ namespace Ais.ImageSplitter.Wpf
         public async Task<SplitResult> SplitAsync(string inputFilePath, string outputFilePath, int[] pageNumbers)
         {
             var result = new SplitResult {InputFilePath = inputFilePath};
-            
-            Stream inputStream = null;
-            Stream outputStream = null;
 
             try
             {
-                inputStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-
-                TiffBitmapDecoder decoder = new TiffBitmapDecoder(inputStream, BitmapCreateOptions.PreservePixelFormat,
-                    BitmapCacheOption.Default);
-
-                TiffBitmapEncoder encoder = new TiffBitmapEncoder();
-
-                foreach (int pageNumber in pageNumbers)
+                await using (Stream inputStream =
+                    new FileStream(inputFilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
                 {
-                    try
-                    {
-                        encoder.Frames.Add(decoder.Frames[pageNumber]);
-                    }
-                    catch (ArgumentOutOfRangeException)
-                    {
-                    }
-                }
+                    var decoder = new TiffBitmapDecoder(inputStream, BitmapCreateOptions.PreservePixelFormat,
+                        BitmapCacheOption.Default);
 
-                if (encoder.Frames.Any())
-                {
-                    outputStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write);
-                    encoder.Save(outputStream);
+                    var encoder = new TiffBitmapEncoder();
+
+                    foreach (int pageNumber in pageNumbers)
+                    {
+                        try
+                        {
+                            encoder.Frames.Add(decoder.Frames[pageNumber]);
+                        }
+                        catch (ArgumentOutOfRangeException)
+                        {
+                        }
+                    }
+
+                    if (encoder.Frames.Any())
+                    {
+                        await using (Stream outputStream =
+                            new FileStream(outputFilePath, FileMode.Create, FileAccess.Write))
+                        {
+                            encoder.Save(outputStream);
+                        }
+                    }
                 }
             }
             catch (Exception e)
             {
                 result.ErrorStatus = e.Message;
                 result.StackTrace = e.StackTrace;
-            }
-            finally
-            {
-                inputStream?.Dispose();
-                outputStream?.Dispose();
             }
 
             return result;
