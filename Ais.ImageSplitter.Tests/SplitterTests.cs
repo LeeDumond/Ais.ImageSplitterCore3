8edbf7cad0b26cc2bb271e56a729b6de2b088af8
Lee Dumond <Lee_Dumond@aisreview.com>
2019-07-31 11:39:33 -0500

    add more tests

---
+++ Ais.ImageSplitter.Tests/SplitterTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Ais.ImageSplitter.Wpf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ais.ImageSplitter.Tests
+{
+    [TestClass]
+    public class SplitterTests
+    {
+        [TestMethod]
+        public async Task SplitAsync_InputFilePathIsSame()
+        {
+            var splitter = new Splitter();
+
+            var result = await splitter.SplitAsync("some input path", null, null);
+
+            Assert.AreEqual("some input path", result.InputFilePath);
+        }
+
+        [TestMethod]
+        public async Task SplitAsync_NullInputPathThrows()
+        {
+            var splitter = new Splitter();
+
+            var result = await splitter.SplitAsync(null, null, null);
+
+            Assert.AreEqual("Path cannot be null. (Parameter 'path')", result.ErrorStatus);
+        }
+    }
+}
