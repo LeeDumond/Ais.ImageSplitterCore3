e23568c8e3958478441587bea92ba6cd484dc77d
Lee Dumond <Lee_Dumond@aisreview.com>
2019-07-31 10:23:29 -0500

    add splitcontroller tests

---
+++ Ais.ImageSplitter.Tests/SplitControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Ais.ImageSplitter.Api.Controllers;
+using Ais.ImageSplitter.Wpf;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Ais.ImageSplitter.Tests
+{
+
+
+    [TestClass]
+    public class SplitControllerTests
+    {
+        [TestMethod]
+        public async Task Post_WithError_StatusCode500()
+        {
+            var mockSplitter = new Mock<ISplitter>();
+            mockSplitter.Setup(s => s.SplitAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int[]>()))
+                .ReturnsAsync( () => new SplitResult{ErrorStatus = "some error message", StackTrace = "some stack trace"});
+
+            var controller = new SplitController(mockSplitter.Object)
+            {
+                ControllerContext = new ControllerContext {HttpContext = new DefaultHttpContext()}
+            };
+
+            ActionResult<SplitResult> result = await controller.Post(new SplitRequest{});
+
+            Assert.AreEqual(500, controller.Response.StatusCode);
+            Assert.AreEqual("some error message", result.Value.ErrorStatus);
+            Assert.AreEqual("some stack trace", result.Value.StackTrace);
+        }
+    }
+}
